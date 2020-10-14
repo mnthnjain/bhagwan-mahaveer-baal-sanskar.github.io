3790958a8cde2333aae436cfd37b5619de14fcdd
manthan kapoor <manthankapoor@microns-MacBook-Pro-4.local>
2020-10-14 13:57:37 +0530

    immportant document updated for cbse affilation

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-if('function' === typeof importScripts) {
-    importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
-    if (workbox) {
-      console.log(`Yay! Workbox is loaded 🎉`);
-    
-      workbox.precaching.precacheAndRoute([]);
-    
-    } else {
-      console.log(`Boo! Workbox didn't load 😬`);
-    }
+if ('function' === typeof importScripts) {
+  importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
+  );
+  if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.precaching.precacheAndRoute([]);
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
   }
-  
+}
